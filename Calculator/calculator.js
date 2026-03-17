0528c4dc36d700cc2a9bdd20f203439cd64356a5
2912qa <andyhome574@gmail.com>
2026-03-16 22:08:28 -0400

    Implement basic calculator functions

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,1 +1,22 @@
+let currentInput = "";
 
+function appendValue(val) {
+    currentInput += val;
+    document.getElementById('result').value = currentInput;
+}
+
+function clearResult() {
+    currentInput = "";
+    document.getElementById('result').value = "";
+}
+
+function calculateResult() {
+    try {
+        // evaluate the string as math
+        currentInput = eval(currentInput).toString();
+        document.getElementById('result').value = currentInput;
+    } catch {
+        document.getElementById('result').value = "Error";
+        currentInput = "";
+    }
+}
